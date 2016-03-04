2cf7da1d9a8a8628d82bd44e6a6e37d7f78d9052
Bart Zaremba <bartosz.zaremba@whallalabs.com>
2016-03-04 19:45:32 +0100

    Add landing page

--- Api/index.js
+++ Api/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
     res.send('Hello World!');
 });
+
+app.use(express.static('../Landingpage'));
 
 app.listen(3000, function () {
     console.log('Server listening on port 3000!');
