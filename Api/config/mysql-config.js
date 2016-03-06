61b17cfa659b9dd50c18feccaa668bfca13068a4
Bart Zaremba <bartosz.zaremba@whallalabs.com>
2016-03-06 20:43:09 +0100

    Add connect to database  mysql

---
+++ Api/config/mysql-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host     : 'ip do wpisania',
+    port: 'port do wpisania',
+    user     : 'root',
+    password : 'haslo do wpisania',
+    database : 'SelfTweak'
+};
--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,23 @@
 var express = require('express');
+var mysql = require('mysql');
+
 var app = express();
+
+var mysqlConfig = require('./config/mysql-config');
+var connection = mysql.createConnection(mysqlConfig);
+connection.connect();
+
+app.get('/users', function (req, res) {
+    connection.query('SELECT * FROM Users', function(err, rows, fields) {
+        if (err) throw err;
+
+        rows.forEach(function (row) {
+            console.log('row:', row);
+        });
+
+        res.send(rows);
+    });
+});
 
 app.get('/hello', function (req, res) {
     res.send('Hello World!');
